18201b64dd1090a52498cce0c16d28c9d209e462
sonnyg <sonny@evagabond.com>
2018-01-18 10:06:05 -0800

    open devtools by default

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
+  mainWindow.toggleDevTools();
+  
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'main.html'),
