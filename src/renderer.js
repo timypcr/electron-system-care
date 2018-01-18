fbef930688453d87161ab2d0a90bb32f5c16c190
sonnyg <sonny@evagabond.com>
2018-01-18 09:55:04 -0800

    adding renderer.js

---
+++ src/renderer.js
@@ -0,0 +1,7 @@
+/*
+  This is the main javascript file for main.html. To simplify things, add this
+  file to the bottom of the webpage. This DOM should be fully instantiated at
+  that point so we don't need on load handlers here.
+*/
+
+console.log('renderer process loaded')
