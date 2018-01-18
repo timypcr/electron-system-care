26e41ff33451c61b257256e9c9accab5ff68be4d
sonnyg <sonny@evagabond.com>
2018-01-18 10:13:48 -0800

    adding click handlers for toggle buttons

--- src/renderer.js
+++ src/renderer.js
@@ -5,3 +5,23 @@
 */
 
 console.log('renderer process loaded')
+
+/*
+  Another method for this event handlers would be to use a single handler, and
+  pass the data in via parameters, like so:
+
+  function handleClick(checked, command) {
+  ...
+  }
+
+  The html would then look like this:
+
+  onclick="handleClick(this.checked, 'command')"
+*/
+function runCommand1(checked) {
+  console.log(`running command 1 - checked: ${checked}`)
+}
+
+function runCommand2(checked) {
+  console.log(`running command 2 - checked: ${checked}`)
+}
